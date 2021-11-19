b4b6741c9e1f11105af64355b23fa7676e9afcc0
amorita3 <amorita3@ivc.edu>
2021-11-18 18:18:56 -0800

    Prototype

--- app/src/main/java/com/example/truthordare/MainActivity.java
+++ app/src/main/java/com/example/truthordare/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
 
@@ -12,17 +13,24 @@ import com.example.truthordare.Fragments.RequestFragment;
 import com.example.truthordare.Fragments.PostsFragment;
 import com.example.truthordare.Fragments.ProfileFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.os.Handler;
+import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity
-{
+public class MainActivity extends AppCompatActivity {
 
+    private static int SPLASH_SCREEN_TIME_OUT=2000;
     final FragmentManager fragmentManager = getSupportFragmentManager();
     private BottomNavigationView bottomNavigationView;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //This method is used so that your splash activity
+        //can cover the entire screen.
+
         setContentView(R.layout.activity_main);
 
 
@@ -54,6 +62,24 @@ public class MainActivity extends AppCompatActivity
         });
         // Set default selection
         bottomNavigationView.setSelectedItemId(R.id.action_home);
+        //this will bind your MainActivity.class file with activity_main.
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i=new Intent(MainActivity.this,
+                        InitialLaunch.class);
+                //Intent is used to switch from one activity to another.
+
+                startActivity(i);
+                //invoke the SecondActivity.
+
+                finish();
+                //the current activity will get finished.
+            }
+        }, SPLASH_SCREEN_TIME_OUT);
+
+
     }
 
 
