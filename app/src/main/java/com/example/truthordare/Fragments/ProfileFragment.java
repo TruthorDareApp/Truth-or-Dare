d0c0ca2a50051b71167c3514972438a6f833aead
BlankMKB <blankmkb@gmail.com>
2021-12-09 13:36:26 -0800

    working compose screen with post elements.

--- app/src/main/java/com/example/truthordare/Fragments/ProfileFragment.java
+++ app/src/main/java/com/example/truthordare/Fragments/ProfileFragment.java
@@ -2,8 +2,6 @@ package com.example.truthordare.Fragments;
 
 import android.os.Bundle;
 
-import androidx.fragment.app.Fragment;
-
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
